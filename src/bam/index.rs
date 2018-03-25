9d199548ec4a1cff9b91338d30a1bdf1d2bcf357
Patrick Marks <pmarks@gmail.com>
2018-03-24 17:27:04 -0700

    wip parsing index

--- src/bam/index.rs
+++ src/bam/index.rs
@@ -1,9 +1,8 @@
-use nom::{HexDisplay,Offset,Needed,IResult,ErrorKind,le_u8,le_u16, le_u32, le_i32, le_u64};
-use nom::Err;
-use std::str::from_utf8;
+use nom::{le_u8, le_u32, le_i32, le_u64};
 
+use std::collections::HashSet;
 
-struct Chunk {
+pub struct Chunk {
     chunk_beg: u64,
     chunk_end: u64
 }
@@ -27,9 +26,9 @@ struct Index {
 named!(parse_ref_index<&[u8], RefIndex>,
     do_parse!(
         n_bin: le_i32 >>
-        bins: many_m_n!(n_bin as usize, n_bin as usize, parse_bin) >>
+        bins: count!(parse_bin, n_bin as usize) >>
         n_intv: le_i32 >>
-        ioffsets: many_m_n!(n_intv as usize, n_intv as usize, le_u64) >>
+        ioffsets: count!(le_u64, n_intv as usize) >>
         (
             RefIndex {
                 bins: bins,
@@ -56,7 +55,7 @@ named!(parse_bin<&[u8], Bin>,
     do_parse!(
         bin: le_u32 >>
         n_chunk: le_i32 >>
-        chunks: many_m_n!(n_chunk as usize, n_chunk as usize, parse_chunk) >>
+        chunks: count!(parse_chunk, n_chunk as usize) >>
         (
             Bin {
                 bin: bin,
@@ -66,13 +65,13 @@ named!(parse_bin<&[u8], Bin>,
     )
 );
 
-const MAGIC: &[u8; 4] = &[b'B', b'A', b'I', 1u8];
+const BAI_MAGIC: &[u8; 4] = &[b'B', b'A', b'I', 1u8];
 
 named!(parse_bai<&[u8], Index>,
     do_parse!(
-        tag!(MAGIC) >> 
+        tag!(BAI_MAGIC) >> 
         n_ref: le_i32 >>
-        ref_indexes: many_m_n!(n_ref as usize, n_ref as usize, parse_ref_index) >>
+        ref_indexes: count!(parse_ref_index, n_ref as usize) >>
         n_no_coor: le_u64 >>
         (
             Index {
@@ -88,48 +87,136 @@ named!(parse_bai<&[u8], Index>,
 pub fn reg2bin(beg: u32, end: u32) -> u32
 {
     let end = end - 1;
-    if (beg>>14 == end>>14) { return ((1<<15)-1)/7 + (beg>>14); }
-    if (beg>>17 == end>>17) { return ((1<<12)-1)/7 + (beg>>17); }
-    if (beg>>20 == end>>20) { return ((1<<9)-1)/7 + (beg>>20); }
-    if (beg>>23 == end>>23) { return ((1<<6)-1)/7 + (beg>>23); }
-    if (beg>>26 == end>>26) { return ((1<<3)-1)/7 + (beg>>26); }
+    if beg>>14 == end>>14 { return ((1<<15)-1)/7 + (beg>>14); }
+    if beg>>17 == end>>17 { return ((1<<12)-1)/7 + (beg>>17); }
+    if beg>>20 == end>>20 { return ((1<<9)-1)/7 + (beg>>20); }
+    if beg>>23 == end>>23 { return ((1<<6)-1)/7 + (beg>>23); }
+    if beg>>26 == end>>26 { return ((1<<3)-1)/7 + (beg>>26); }
     return 0;
 }
 /* calculate the list of bins that may overlap with region [beg,end) (zero-based) */
-const MAX_BIN: u32 = (((1<<18)-1)/7);
+const MAX_BIN: usize = (((1<<18)-1)/7);
 
-pub fn reg2bins(beg: u32, end: u32, list: &mut [u16; MAX_BIN])
+pub fn reg2bins(beg: u32, end: u32, bins: &mut Vec<u16>)
 {
-    let mut i = 0;
     let end = end - 1;
-
-    list[i] = 0;
-    i += 1;
+    bins.clear();
+    bins.push(0);
 
     for k in 1 + (beg>>26) .. 1 + (end>>26) {
-        list[i] = k;
-        i += 1;
+        bins.push(k as u16);
     }
 
     for k in  9 + (beg>>23) ..  9 + (end>>23) {
-        list[i] = k;
-        i += 1;
+        bins.push(k as u16);
     }
 
     for k in 73 + (beg>>20) .. 73 + (end>>20) {
-        list[i] = k;
-        i += 1;
+        bins.push(k as u16);
     }
 
     for k in 585 + (beg>>17) .. 585 + (end>>17) {
-        list[i] = k;
-        i += 1;
+        bins.push(k as u16);
     }
 
     for k in 4681 + (beg>>14) .. 4681 + (end>>14) {
-        list[i] = k;
-        i += 1;
+        bins.push(k as u16);
     }
+}
 
-    return i;
+
+struct CsiBin {
+    bin: u32,
+    loffset: u64,
+    chunks: Vec<Chunk>,
+}
+
+struct CsiRefIndex {
+    bins: Vec<CsiBin>,
+}
+
+struct CsiIndex {
+    min_shift: i32,
+    depth: i32,
+    aux: Vec<u8>,
+    ref_indexes: Vec<CsiRefIndex>,
+    n_no_coor: u64,
+}
+
+const CSI_MAGIC: &[u8; 4] = &[b'C', b'S', b'I', 1u8];
+
+named!(parse_csi<&[u8], CsiIndex>,
+    do_parse!(
+        tag!(CSI_MAGIC) >> 
+        min_shift: le_i32 >>
+        depth: le_i32 >>
+        l_aux: le_i32 >>
+        aux: count!(le_u8, l_aux as usize) >>
+        n_ref: le_i32 >>
+        ref_indexes: count!(parse_csi_ref_index, n_ref as usize) >>
+        n_no_coor: le_u64 >>
+        (
+            CsiIndex {
+                min_shift: min_shift,
+                depth: depth,
+                aux: aux,
+                ref_indexes: ref_indexes,
+                n_no_coor: n_no_coor,
+            }
+        )
+    )
+);
+
+
+named!(parse_csi_ref_index<&[u8], CsiRefIndex>,
+    do_parse!(
+        n_bin: le_i32 >>
+        bins: count!(parse_csi_bin, n_bin as usize) >>
+        (
+            CsiRefIndex {
+                bins: bins,
+            }
+        )
+    )
+);
+
+named!(parse_csi_bin<&[u8], CsiBin>,
+    do_parse!(
+        bin: le_u32 >>
+        loffset: le_u64 >>
+        n_chunk: le_i32 >>
+        chunks: count!(parse_chunk, n_chunk as usize) >>
+        (
+            CsiBin {
+                bin: bin,
+                loffset: loffset,
+                chunks: chunks,
+            }
+        )
+    )
+);
+
+pub trait BgzfIndex {
+    fn chunks(&self, ref_id: usize, beg: u32, end: u32, chunks: &mut Vec<Chunk>);
+}
+
+use std::iter::FromIterator;
+
+impl BgzfIndex for Index {
+    fn chunks(&self, ref_id: usize, beg: u32, end: u32, chunks: &mut Vec<Chunk>) {
+        let mut bins = Vec::new();
+        reg2bins(beg, end, &mut bins);
+        let bins: HashSet<u16> = HashSet::from_iter(bins.into_iter());
+
+        let ref_idx = &self.ref_indexes[ref_id];
+
+        for b in ref_idx.bins.iter() {
+            if bins.contains(&(b.bin as u16)) {
+
+                
+            }
+        }
+
+
+    }
 }
